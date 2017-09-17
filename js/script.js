f9ee9d8705dcabe61f708bd647f92c5e131bcf43
michu1234 <moooderator@gmail.com>
2017-09-17 04:29:50 +0200

    ver 2.0

--- js/script.js
+++ js/script.js
@@ -22,9 +22,35 @@ $(".nav__list li:nth-child(5)").click(function() {
   }, 2000);
 });
 
+$('.button-up').hide();
+
+$(document).scroll(function() {
+  var y = $(this).scrollTop();
+  if (y > 2000) {
+    $('.button-up').fadeIn();
+  } else {
+    $('.button-up').fadeOut();
+  }
+});
+
 $(".button-up").click(function() {
   $('html, body').animate({
     scrollTop: $(".nav__list li:first-child").offset().top
   }, 2000);
-  $(this).delay(500).fadeOut()
 });
+
+$(".btn--arrow").click(function() {
+  $('html, body').animate({
+    scrollTop: $(".apps").offset().top
+  }, 1500);
+});
+
+
+
+
+$('.left').click(function(){
+  var firstThing = $('.apps__item:first-child').clone();
+
+  $(firstThing).before($('apps__item:last-child'));
+  $('.apps__examples').animate({'marginLeft': "-=650px"});
+})
